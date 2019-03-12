c1a35861b2e89109113c5f0f4dc42045073ec18a
Kumar <kiran.1.kumar@nokia.com>
2019-03-12 22:28:25 +0530

    Updates

---
+++ precache-manifest.c4fbc0554263d90d730ff2ffbf96beee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BMS-Challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "29db5cee4fcfbeb6b78f",
+    "url": "/BMS-Challenge/static/js/runtime~main.0b467c6a.js"
+  },
+  {
+    "revision": "77868b0507154ca4d413",
+    "url": "/BMS-Challenge/static/js/main.d9f3d47f.chunk.js"
+  },
+  {
+    "revision": "c042a502c13e003752aa",
+    "url": "/BMS-Challenge/static/js/2.87e42b35.chunk.js"
+  },
+  {
+    "revision": "77868b0507154ca4d413",
+    "url": "/BMS-Challenge/static/css/main.9161ec3d.chunk.css"
+  },
+  {
+    "revision": "a14812b762bd259965018e471f8273bc",
+    "url": "/BMS-Challenge/index.html"
+  }
+];
